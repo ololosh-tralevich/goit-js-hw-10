3dc960183f517147560544bc63d409f23a7e3350
ololosh-tralevich <karpeko76@gmail.com>
2022-01-25 13:21:36 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
+import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const textField = document.querySelector('input#search-box');
+const countryList = document.querySelector('ul.country-list');
+const countryBlock = document.querySelector('div.country-info');
 const DEBOUNCE_DELAY = 300;
 // Notify.failure('');
 // Notify.info('');
 
+// console.log(countryList)
+
 textField.addEventListener(
   'input',
   debounce(() => {
-      
+    let countryName = textField.value;
+    console.log(textField.value);
+
+    fetchCountries(countryName)
+      .then(data => {
+        if (data.length >= 10) {
+          Notify.info('Too many matches found. Please enter a more specific name.');
+          return;
+        } else if (data.length >= 2) {
+          for (let country of data) {
+            console.log(country);
+            countryList.insertAdjacentHTML('beforeend',
+              `<li>
+              <img src="${country.flags.svg}" width="40px"></img>
+              <p>${country.name.common}</p>
+            </li>`,
+            );
+          }
+          return;
+        }
+      })
+      .catch(err => {
+        // Notify.failure('Oops, there is no country with that name.');
+        console.log(err);
+      });
   }, DEBOUNCE_DELAY),
 );
