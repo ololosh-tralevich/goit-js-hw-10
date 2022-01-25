4c7b30967291cad2611c0c13f09cc2a29f45cd74
ololosh-tralevich <karpeko76@gmail.com>
2022-01-25 14:52:10 +0200

    done task 10

--- src/index.js
+++ src/index.js
@@ -7,37 +7,68 @@ const textField = document.querySelector('input#search-box');
 const countryList = document.querySelector('ul.country-list');
 const countryBlock = document.querySelector('div.country-info');
 const DEBOUNCE_DELAY = 300;
-// Notify.failure('');
-// Notify.info('');
-
-// console.log(countryList)
 
 textField.addEventListener(
   'input',
   debounce(() => {
     let countryName = textField.value;
-    console.log(textField.value);
+    console.log(textField.value)
+    if (!textField.value) {
+      countryList.innerHTML = '';
+      countryBlock.innerHTML = '';
+      return;
+    }
 
     fetchCountries(countryName)
-      .then(data => {
+    .then(data => {
+        countryList.innerHTML = '';
+        countryBlock.innerHTML = '';
+
         if (data.length >= 10) {
           Notify.info('Too many matches found. Please enter a more specific name.');
           return;
         } else if (data.length >= 2) {
           for (let country of data) {
             console.log(country);
-            countryList.insertAdjacentHTML('beforeend',
+            countryList.insertAdjacentHTML(
+              'beforeend',
               `<li>
               <img src="${country.flags.svg}" width="40px"></img>
-              <p>${country.name.common}</p>
+              <h2>${country.name.common}</h2>
             </li>`,
             );
           }
           return;
         }
+
+        const countryLanguages = Object.values(data[0].languages).join(', ');
+        countryBlock.insertAdjacentHTML(
+          'beforeend',
+          `<div class="country-header">
+            <img src="${data[0].flags.svg}" height="35px"></img>
+            <h1>${data[0].name.common}</h1>
+            </div>
+            <ul>
+              <li class="country-info-list">
+                <h3>Capital:</h3>
+                <p>${data[0].capital}</p>
+              </li>
+              <li class="country-info-list">
+                <h3>Population:</h3>
+                <p>${data[0].population}</p>
+              </li>
+              <li class="country-info-list">
+                <h3>Languages:</h3>
+                <p>${countryLanguages}</p>
+              </li>
+            </ul>
+          `,
+        );
       })
       .catch(err => {
-        // Notify.failure('Oops, there is no country with that name.');
+        Notify.failure('Oops, there is no country with that name.');
+        countryList.innerHTML = '';
+
         console.log(err);
       });
   }, DEBOUNCE_DELAY),
